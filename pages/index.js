c28da6cd5ab6d086a0ceced923b13bb3d8be2a89
theend123456 <105260770+theend123456@users.noreply.github.com>
2022-05-17 15:55:34 +0300

    news app

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import  {Toolbar}  from "../components/toolbar";
+import { useRouter } from "next/router";
+
+
+
+export default function Home() {
+  return (
+   
+    <div className="page-container">
+
+      <Toolbar/>
+      <div className={styles.main}>
+        <h1> Next.js News App</h1>
+
+
+        <h3>Your one stop shop for the latest new articles</h3>
+      </div>
+    </div>
+  );
+}
