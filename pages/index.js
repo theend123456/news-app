dd0bd66014d0648fc7bf299571c905114aef933c
theend123456 <105260770+theend123456@users.noreply.github.com>
2022-05-17 16:45:59 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <Toolbar/>
       <div className={styles.main}>
-        <h1> Next.js Haberler</h1>
+        <h1> Next.js Haberler git</h1>
 
 
         <h3>Your one stop shop for the latest new articles</h3>
